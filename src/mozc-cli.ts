8626e854ac4845babafe97fe29647b4e5bee0a77
Kay Gosho <ketsume0211@gmail.com>
2021-12-17 07:09:09 +0900

    test with sway ipc

--- src/mozc-cli.ts
+++ src/mozc-cli.ts
@@ -1,16 +1,55 @@
 import { Socket } from 'net'
+import net from 'net'
 import path from 'path'
 import os from 'os'
 
 async function main() {
-  const sock = new Socket({
-    readable: true,
-    writable: true,
+  // const sock = new Socket({
+  //   readable: true,
+  //   writable: true,
+  // })
+
+  const socket = net.createConnection(
+    // path.resolve(os.homedir(), '.mozc/.session.ipc'),
+    '/run/user/1000/sway-ipc.1000.1424.sock',
+    () => {
+      console.log('connected')
+    },
+  )
+  socket.on('error', (e) => {
+    console.log(e)
   })
+  socket.on('data', (data) => {
+    console.log(String(data))
+  })
+  socket.write(format(4))
+  setTimeout(() => {
+    socket.destroy()
+  }, 1000)
+}
 
-  sock.connect(path.resolve(os.homedir(), '.mozc/.session.ipc'))
+var constants = {
+  MAGIC_STRING: 'i3-ipc',
+}
 
-  sock
+function format(type: number) {
+  const size = 0
+  let msg = constants.MAGIC_STRING
+  msg += packInt(size, type)
+  return msg
+}
+
+// https://github.com/badboy/node-i3/blob/master/lib/node-i3.js
+function packInt(size: number, type: number) {
+  const args = Array.prototype.slice.call([size, type])
+  let result = ''
+  args.forEach(function (e) {
+    result += String.fromCharCode(e & 0xff)
+    result += String.fromCharCode((e >> 8) & 0xff)
+    result += String.fromCharCode((e >> 16) & 0xff)
+    result += String.fromCharCode((e >> 24) & 0xff)
+  })
+  return result
 }
 
 main()
