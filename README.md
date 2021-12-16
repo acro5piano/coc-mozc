00b2270436441738cdfd087e9c9706520b39cc9d
Kay Gosho <ketsume0211@gmail.com>
2021-12-16 11:13:34 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# coc-mozc
+
+mozc input method source for coc.nvim
+
+## Install
+
+`:CocInstall coc-mozc`
+
+## Keymaps
+
+`nmap <silent> <C-l> <Plug>(coc-coc-mozc-keymap)`
+
+## Lists
+
+`:CocList demo_list`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { commands, CompleteResult, ExtensionContext, listManager, sources, window, workspace } from 'coc.nvim';
+import DemoList from './lists';
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  window.showMessage(`coc-mozc works!`);
+
+  context.subscriptions.push(
+    commands.registerCommand('coc-mozc.Command', async () => {
+      window.showMessage(`coc-mozc Commands works!`);
+    }),
+
+    listManager.registerList(new DemoList(workspace.nvim)),
+
+    sources.createSource({
+      name: 'coc-mozc completion source', // unique id
+      doComplete: async () => {
+        const items = await getCompletionItems();
+        return items;
+      },
+    }),
+
+    workspace.registerKeymap(
+      ['n'],
+      'mozc-keymap',
+      async () => {
+        window.showMessage(`registerKeymap`);
+      },
+      { sync: false }
+    ),
+
+    workspace.registerAutocmd({
+      event: 'InsertLeave',
+      request: true,
+      callback: () => {
+        window.showMessage(`registerAutocmd on InsertLeave`);
+      },
+    })
+  );
+}
+
+async function getCompletionItems(): Promise<CompleteResult> {
+  return {
+    items: [
+      {
+        word: 'TestCompletionItem 1',
+        menu: '[coc-mozc]',
+      },
+      {
+        word: 'TestCompletionItem 2',
+        menu: '[coc-mozc]',
+      },
+    ],
+  };
+}
